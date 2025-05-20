586cdbd5a7fb2478e41ca48b21eb8d912e12a0e9
junelstone <35089069+junelstone@users.noreply.github.com>
2025-05-20 12:49:17 +0200

    Add files via upload

--- 1hse_form_app.py
+++ 1hse_form_app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template_string, request, send_file
 from fpdf import FPDF
 import io
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 form_template = """
 <!DOCTYPE html>
@@ -27,8 +27,8 @@ form_template = """
         
         <label>Département concerné:</label><br>
         <input type="text" name="departement"><br><br>
-
- <label>Commentaires généraux:</label><br>
+        
+        <label>Commentaires généraux:</label><br>
         <textarea name="commentaires" rows="5" cols="40"></textarea><br><br>
         
         <label>Signature de l’inspecteur:</label><br>
@@ -55,25 +55,26 @@ def generate_pdf():
     pdf.add_page()
     pdf.set_font("Arial", size=12)
 
- fields = [
-        ("Date de l’inspection", data['date_inspection']),
-        ("Lieu / Site", data['lieu']),
-        ("Installation", data['installation']),
-        ("Inspecteur(s)", data['inspecteurs']),
-        ("Département concerné", data['departement']),
-        ("Commentaires généraux", data['commentaires']),
-        ("Signature de l’inspecteur", data['signature_inspecteur']),
-        ("Signature du responsable de site", data['signature_responsable']),
+    fields = [
+        ("Date de l’inspection", data.get('date_inspection', '')),
+        ("Lieu / Site", data.get('lieu', '')),
+        ("Installation", data.get('installation', '')),
+        ("Inspecteur(s)", data.get('inspecteurs', '')),
+        ("Département concerné", data.get('departement', '')),
+        ("Commentaires généraux", data.get('commentaires', '')),
+        ("Signature de l’inspecteur", data.get('signature_inspecteur', '')),
+        ("Signature du responsable de site", data.get('signature_responsable', '')),
     ]
 
     for label, value in fields:
         pdf.multi_cell(0, 10, f"{label}: {value}")
 
     pdf_output = io.BytesIO()
-    pdf.output(pdf_output)
+    pdf_bytes = pdf.output(dest='S').encode('latin1')
+    pdf_output.write(pdf_bytes)
     pdf_output.seek(0)
 
     return send_file(pdf_output, as_attachment=True, download_name="fiche_HSE.pdf", mimetype='application/pdf')
 
-app.run(debug=False)
-
+if name == '__main__':
+    app.run(debug=True)
